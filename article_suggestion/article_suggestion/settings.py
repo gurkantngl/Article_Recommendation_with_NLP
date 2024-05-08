deef007cd8e4c65ecaa9c455b9ab18ae5b3a8cf7
gurkantngl <gurkantngl@gmail.com>
2024-05-08 15:34:16 +0300

    django settings update

--- article_suggestion/article_suggestion/settings.py
+++ article_suggestion/article_suggestion/settings.py
@@ -73,12 +73,25 @@ WSGI_APPLICATION = 'article_suggestion.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'djongo',
+        'NAME': 'mydatabase',
+        'ENFORCE_SCHEMA': False,
+        'CLIENT': {
+            'host': 'mongodb://localhost:27017',
+        }
     }
 }
+
 
 
 # Password validation
